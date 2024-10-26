f552fee6458227b1c9f15223a95202435a26e13e
Amirhossein Motaghian <a.motaghian@esavis.ir>
2024-10-26 12:25:10 +0330

    :hammer: chore: generate app and bootstrap command

---
+++ cmd/app/app.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"GoAuth/src/bootstrap"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+// App Commands for interacting with apps
+var App = &cobra.Command{
+	Use:   "app",
+	Short: "Commands for interacting with apps.",
+}
+
+func init() {
+	App.AddCommand(
+		bootstrapCmd,
+	)
+}
+
+// bootstrapCmd is sub command for App that bootstrap the application
+// usage: go run main.go app bootstrap or ./binary app bootstrap
+var bootstrapCmd = &cobra.Command{
+	Use:   "bootstrap",
+	Short: "Bootstraps the application and it's related services.",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := bootstrap.Init(); err != nil {
+			log.Fatalf("Bootstrap Service: Failed to Initialize. %v", err)
+		}
+	},
+}
