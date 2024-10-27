671036a371a279f9befe81450e2ecf86ad39eec5
amir <amirmemool12@gmail.com>
2024-10-27 14:36:38 -0400

    :hammer: chore: update user model

--- src/models/user.go
+++ src/models/user.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string `json:"first_name" gorm:"type:varchar(20);not null"`
-	LastName  string `json:"last_name" gorm:"type:varchar(20);not null"`
-	Email     string `json:"email" gorm:"type:varchar(20);not null"`
+	FirstName string `json:"first_name" gorm:"type:varchar(64);not null"`
+	LastName  string `json:"last_name" gorm:"type:varchar(64);not null"`
+	Email     string `json:"email" gorm:"type:varchar(64);not null"`
 	Password  string `json:"password" gorm:"type:varchar(255);not null"`
 }
 
