aa2bab0ed971db087f48cfd0e96c250cf385f298
amir <amirmemool12@gmail.com>
2024-10-29 12:04:30 -0400

    :hammer: chore: add type in userTable

--- src/models/user.go
+++ src/models/user.go
@@ -6,12 +6,21 @@ import (
 	"time"
 )
 
+type UserType string
+
+var (
+	SuperAdmin UserType = "super-admin"
+	Admin      UserType = "admin"
+	SimpleUser UserType = "user"
+)
+
 type User struct {
 	ID        uint           `gorm:"primarykey"`
 	FirstName string         `json:"first_name" gorm:"type:varchar(64);not null"`
 	LastName  string         `json:"last_name" gorm:"type:varchar(64);not null"`
 	Email     string         `json:"email" gorm:"type:varchar(64);not null;unique"`
 	Password  string         `json:"-" gorm:"type:varchar(255);not null"`
+	Type      UserType       `json:"type" gorm:"type:varchar(255);not null;default=user"`
 	Tokens    []*Token       `json:"-" gorm:"constraint:OnDelete:CASCADE;"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
